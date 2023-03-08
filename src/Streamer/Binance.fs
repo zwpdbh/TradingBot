f9647a821ce717703b904347e91c08bfe984a1b8
zwpdbh <hyperion_z@outlook.com>
2023-03-08 23:06:57 +0800

    <finish chapter 01

--- src/Streamer/Binance.fs
+++ src/Streamer/Binance.fs
@@ -1,11 +1,77 @@
 ﻿namespace Streamer 
 
 module Binance = 
-    // How to use websocket to do something similar from
-    // https://book.elixircryptobot.com/stream-live-cryptocurrency-prices-from-the-binance-wss.html#create-a-supervised-application-inside-an-umbrella
     open Websocket.Client
     open System
+    open Thoth.Json.Net
+    open Tools.Logging
+    type TradeEvent =
+        {
+            eventType: string 
+            eventTime: DateTime
+            symbol: string 
+            tradeId: int 
+            price: decimal
+            quantity: decimal 
+            buyerOrderId: int 
+            sellerOrderId: int 
+            tradeTime: DateTime 
+            buyerMarketMarker: bool
+            ignore: bool
+        }
 
+
+    
+    //{
+    //  "e": "trade",       // Event type
+    //  "E": 1672515782136, // Event time
+    //  "s": "BNBBTC",      // Symbol
+    //  "t": 12345,         // Trade ID
+    //  "p": "0.001",       // Price
+    //  "q": "100",         // Quantity
+    //  "b": 88,            // Buyer order ID
+    //  "a": 50,            // Seller order ID
+    //  "T": 1672515782136, // Trade time
+    //  "m": true,          // Is the buyer the market maker?
+    //  "M": true           // Ignore
+    //}
+
+
+    let dateDecoder: Decoder<DateTime> = 
+        Decode.int64
+        |> Decode.andThen (fun unixTimestamp ->
+            Decode.succeed (DateTimeOffset.FromUnixTimeMilliseconds(unixTimestamp).DateTime)
+        )
+
+    let (|GetTradeEvent|_|) str: option<TradeEvent> = 
+        let tradeEventDecoder: Decoder<TradeEvent> = 
+
+            Decode.object (
+                fun get -> 
+                    {
+                        TradeEvent.eventType = get.Required.Field "e" (Decode.string)
+                        TradeEvent.eventTime = get.Required.Field "E" dateDecoder
+                        TradeEvent.symbol = get.Required.Field "s" (Decode.string)
+                        TradeEvent.tradeId = get.Required.Field "t" (Decode.int)
+                        TradeEvent.price = get.Required.Field "p" (Decode.decimal)
+                        TradeEvent.quantity = get.Required.Field "q" (Decode.decimal)
+                        TradeEvent.buyerOrderId = get.Required.Field "b" (Decode.int)
+                        TradeEvent.sellerOrderId = get.Required.Field "a" (Decode.int)
+                        TradeEvent.tradeTime = get.Required.Field "T" dateDecoder
+                        TradeEvent.buyerMarketMarker = get.Required.Field "m" (Decode.bool)
+                        TradeEvent.ignore = get.Required.Field "M" (Decode.bool)
+                    }
+            )
+
+        match str |> Decode.fromString tradeEventDecoder with 
+        | Ok data -> Some data 
+        | Error err -> 
+            log 0 $"decode err: {err}"
+            None
+
+
+    // How to use websocket to do something similar from
+    // https://book.elixircryptobot.com/stream-live-cryptocurrency-prices-from-the-binance-wss.html#create-a-supervised-application-inside-an-umbrella
     let endpoint = "wss://stream.binance.us:9443/ws/"
 
     let tradeUrl (symbol: string) = 
@@ -15,13 +81,19 @@ module Binance =
     let processMessage (msg: ResponseMessage) = 
         // It received something like:
         //{"e":"trade","E":1678281305803,"s":"ETHUSD","t":44172879,"p":"1549.38000000","q":"0.05300000","b":1478766161,"a":1479008752,"T":1678281305802,"m":true,"M":true}
-        printfn $"Received message: {msg}"
+        //printfn $"Received message: {msg}"
+        match msg.ToString() with 
+        | GetTradeEvent event -> 
+            log 3 $"Trade event received: {event.symbol}@{event.price}"
+        | msg -> 
+            log 0 $"Unknown event: {msg}"
+
 
     let processReconnection (msg: Models.ReconnectionInfo) = 
-        printfn $"Reconnection happened, type: {msg.Type}"
+        log 1 $"Reconnection happened, type: {msg.Type}"
 
     let processDisconnection (msg: DisconnectionInfo) = 
-        printfn $"disconnected: {msg}"
+        log 1 $"disconnected: {msg}"
 
     let rec receiveMessage (ws: WebsocketClient) =       
         async {
@@ -50,48 +122,5 @@ module Binance =
         runClient ws |> Async.RunSynchronously
 
         
-
-
-//module Binance = 
-//    open System
-//    open Websocket.Client
-
-//    let endpoint = "wss://stream.binance.us:9443/ws/"
-//    let url = endpoint + "ethusd" + "@trade"
-    
-//    let ws = new WebsocketClient(new Uri(url))
-
-//    ws.DisconnectionHappened.Subscribe (fun msg -> 
-//        printfn $"disconnected: {msg}"
-//    ) |> ignore
-
-//    ws.ReconnectTimeout <- TimeSpan.FromSeconds(300)
-
-//    ws.ReconnectionHappened.Subscribe(fun msg ->
-//        printfn $"Reconnection happened, type: {msg.Type}") |> ignore
-
-//    ws.MessageReceived.Subscribe(fun msg ->
-//        printfn $"Received message: {msg}"
-//    ) |> ignore 
-
-//    let rec receiveMessage (ws: WebsocketClient) = 
-//        // It received something like:
-//        //{"e":"trade","E":1678281305803,"s":"ETHUSD","t":44172879,"p":"1549.38000000","q":"0.05300000","b":1478766161,"a":1479008752,"T":1678281305802,"m":true,"M":true}
-//        async {
-//            do! receiveMessage ws
-//        }
-
-//    let startStreaming () = 
-//        let runClient() = 
-//            async {
-                
-//                do! ws.Start() |> Async.AwaitTask
-//                do! [
-//                        receiveMessage ws 
-//                    ] |> Async.Parallel |> Async.Ignore
-//            }
-
-//        runClient () |> Async.RunSynchronously
-    
         
 
