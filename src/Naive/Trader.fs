518e2e7bb6be1e4531ca2ea38b46a0e26ed8bbbb
zwpdbh <hyperion_z@outlook.com>
2023-03-10 09:01:30 +0800

    > learn how to convert elixir example to fsharp

--- src/Naive/Trader.fs
+++ src/Naive/Trader.fs
@@ -2,6 +2,45 @@
 
 module Trader = 
     open Streamer
+    open Tools.Logging  
+
+    type Order = 
+        {
+            price: decimal
+            orderId: int 
+            orderQty: int 
+        }
+
+    type TradeOrder = 
+        | Buy of Order
+        | Sell of Order
+
+    type TradeState = 
+        {
+            symbol: string 
+            profitInterval: int 
+            tickSize: int 
+            order: TradeOrder option
+        }
+
+    let fetchTickSize (symbol: string) = 
+        // TODO use real API
+        10
+
+    //let init (symbol: string) (profileInterval: int) =
+    //    let symbol = symbol.ToUpper()
+    //    log 3 $"Initializing new trader for ${symbol}"
+
+    //    let tick_size = fetchTickSize(symbol)
+
+
+
+    let rec tradeLoop (tradeEvent: Binance.TradeEvent) (state: TradeState) = 
+        async {
+
+
+            return! tradeLoop(tradeEvent, resul)
+        }
 
     let start () = 
         Binance.startStreaming "ethusd"
